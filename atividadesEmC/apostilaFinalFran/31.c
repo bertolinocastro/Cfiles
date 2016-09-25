d9e9dc8c78f4e75399da2472667fddfc1e1f5937
Bertolino <jgabbc@hotmail.com>
2016-09-24 21:34:55 -0300

    Finalizacao Apostila Final Fran

--- atividadesEmC/apostilaFinalFran/31.c
+++ atividadesEmC/apostilaFinalFran/31.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
+unsigned int i ;
 int main(void){
 
-    unsigned int Ndiscos , i;
+    void hanoi( unsigned int n , char Ori , char Dest , char Aux );
+    unsigned int Ndiscos;
 
     scanf( "%u" , &Ndiscos );
 
-    void hanoi( unsigned int n , char Ori , char Dest , char Aux ){
-        if( n == 1 ){
-            printf( "Movendo disco %u do pilar %c para o %c\n" , n , Ori , Dest);
-        }else{
-            hanoi( n - 1 , Ori , Aux , Dest );
-            printf( "Movendo disco %u do pilar %c para o %c\n" , n , Dest , Aux );
-            hanoi( n - 1 , Ori , Dest , Aux );
-        }
-    }
     hanoi( Ndiscos , '1' , '2' , '3' );
+    printf( "Número total de movimentos: %u\n" , i );
 
     return 0;
+}
+
+void hanoi( unsigned int n , char Ori , char Dest , char Aux ){
+    if( n <= 1 ){
+        printf( "Movendo disco %u do pilar %c para o %c\n" , n , Ori , Dest);
+        ++i;
+    }else{
+        hanoi( n - 1 , Ori , Aux , Dest );
+        printf( "Movendo disco %u do pilar %c para o %c\n" , n , Ori , Dest );
+        ++i;
+        hanoi( n - 1 , Aux , Dest , Ori );
+    }
 }
