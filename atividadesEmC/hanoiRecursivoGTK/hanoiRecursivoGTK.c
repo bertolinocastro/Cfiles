cfd548495acc1e617422a01d1d7d04703a2b72f5
Bertolino <jgabbc@hotmail.com>
2016-09-26 23:49:24 -0300

    Gtk animado!

--- atividadesEmC/hanoiRecursivoGTK/hanoiRecursivoGTK.c
+++ atividadesEmC/hanoiRecursivoGTK/hanoiRecursivoGTK.c
@@ -5,13 +5,18 @@
 unsigned int i = 0 , j = 0;
 unsigned int Ndiscos;
 
+/* Declaracoes para hanoi() */
 void hanoi( unsigned int n , unsigned int Ori , unsigned int Dest , unsigned int Aux );
 
 /* Declarações para interfaceGraf(); */
 void interfaceGraf( int argc , char **argv );
-GtkWidget *janela , *grade , **pilar, **disco , *espacadorV ;
+GtkWidget *janela , *grade , **pilar, **disco , *espacadorV , *botao;
 GdkRGBA color , corDiscos;
 unsigned int qtdRows;
+
+void fecharJanela( GtkWidget *w , gpointer p );
+
+void passo( GtkWidget *w , gpointer p );
 
 void moveDisco( unsigned int numDisco , unsigned int pilarOri , unsigned int pilarDest );
 
@@ -38,11 +43,13 @@ int main( int argc , char **argv ){
 void hanoi( unsigned int n , unsigned int Ori , unsigned int Dest , unsigned int Aux ){
     if( n <= 1 ){
         printf( "Movendo disco %u do pilar %u para o %u\n" , n , Ori , Dest);
+        sleep( 1 );
         moveDisco( n - 1 , Ori , Dest );
         ++j;
     }else{
         hanoi( n - 1 , Ori , Aux , Dest );
         printf( "Movendo disco %u do pilar %u para o %u\n" , n , Ori , Dest );
+        sleep( 1 );
         moveDisco( n - 1 , Ori , Dest );
         ++j;
         hanoi( n - 1 , Aux , Dest , Ori );
@@ -50,7 +57,6 @@ void hanoi( unsigned int n , unsigned int Ori , unsigned int Dest , unsigned int
 }
 
 void moveDisco( unsigned int numDisco , unsigned int pilarOri , unsigned int pilarDest ){
-
     gpointer auxiliar;
     auxiliar = g_object_ref( GTK_WIDGET ( disco[numDisco] ) );
     gtk_container_remove( GTK_CONTAINER ( grade ) , disco[numDisco] );
@@ -59,12 +65,14 @@ void moveDisco( unsigned int numDisco , unsigned int pilarOri , unsigned int pil
 
     --discos_existentes.pilar[pilarOri-1];
     ++discos_existentes.pilar[pilarDest-1];
-
+/*
     desespacarVertical( pilarOri - 1 );
     desespacarVertical( pilarDest - 1 );
     espacarVertical( pilarOri - 1 );
     espacarVertical( pilarDest - 1 );
-
+*/
+    gtk_widget_queue_draw( GTK_WIDGET ( disco[numDisco] ) );
+    while ( g_main_context_iteration( NULL , FALSE ) );
 }
 
 void espacarVertical( unsigned int pilarAtual ){
@@ -79,6 +87,15 @@ void desespacarVertical( unsigned int pilarAtual ){
     for( i = qtdRows - discos_existentes.pilar[pilarAtual] ; i < qtdRows ; ++i , printf( "Limpeza: %u -> pilatual %u\n" , i - 1 , pilarAtual ) ) gtk_widget_set_margin_top( GTK_WIDGET ( gtk_grid_get_child_at( GTK_GRID ( grade ) , pilarAtual * 10 , i ) ) , 0 );
 }
 
+void fecharJanela( GtkWidget *w , gpointer p ){
+    gtk_main_quit();
+}
+
+void chamahanoi( GtkWidget *w , gpointer p ){
+    /* Chamada para a função recursiva da torre de Hanói */
+    hanoi( Ndiscos , 1 , 2 , 3 );
+}
+
 void interfaceGraf( int argc , char **argv ){
 
     unsigned int altPilar = 20 * Ndiscos + 40 ;
@@ -90,10 +107,16 @@ void interfaceGraf( int argc , char **argv ){
     disco = malloc( Ndiscos * sizeof( GtkWidget * ) );
     pilar = malloc( 3 * sizeof( GtkWidget * ) );
 
+    botao = gtk_button_new_with_label( "Inicio" );
+    g_signal_connect( GTK_BUTTON ( botao ) , "clicked" , G_CALLBACK ( chamahanoi ) , NULL );
+
     /* Criação da janela como um todo */
     janela = gtk_window_new( GTK_WINDOW_TOPLEVEL );
     gtk_window_set_title( GTK_WINDOW (janela) , "Torre de Hanoi" );
     /*gtk_window_set_default_size( GTK_WINDOW (janela) , 800 , 600 );*/
+
+    /* Função para finalizar a execução caso fechar a janela */
+    g_signal_connect( GTK_WINDOW ( janela ) , "destroy" , G_CALLBACK ( fecharJanela ) , NULL );
 
     /* Criação do grid que irá conter os elementos (pinos) */
     grade = gtk_grid_new();
@@ -108,7 +131,7 @@ void interfaceGraf( int argc , char **argv ){
     for( i = 0 ; i < 30 ; ++i ) gtk_grid_insert_column( GTK_GRID ( grade ) , i );
 
     /* Criação dos widgets (pilares) */
-    for( i = 0 ; i < 3 ; ++i ) pilar[i] = gtk_widget_new( GTK_TYPE_LABEL , "label" , "" /*, "xalign" , 0.0 */, NULL );
+    for( i = 0 , letra = '1' ; i < 3 ; ++i , ++letra ) pilar[i] = gtk_widget_new( GTK_TYPE_LABEL , "label" , &letra /*, "xalign" , 0.0 */, NULL );
 
     /* Margeamento à esquerda dos pilares */
     for( i = 0 ; i < 3 ; ++i ) ( gtk_widget_set_margin_start( GTK_WIDGET ( pilar[i] ) , 50 ) , gtk_widget_set_margin_end( GTK_WIDGET ( pilar[i] ) , 50 ) );
@@ -144,8 +167,8 @@ void interfaceGraf( int argc , char **argv ){
     /* Primeira chamada à função de espacar verticalmente */
     espacarVertical( 0 );
 
-    /* Chamada para a função recursiva da torre de Hanói */
-    hanoi( Ndiscos , 1 , 2 , 3 );
+    /* Inserindo botão */
+    gtk_grid_attach( GTK_GRID ( grade ) , botao , 0 , 0 , 1 , 1 );
 
     /* Inserção dos containers internos à janela */
     gtk_container_add( GTK_CONTAINER( janela ) , grade );
