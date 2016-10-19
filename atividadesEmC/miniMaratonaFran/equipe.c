b1af5dd772bc73c0702ba75fbab41815fee844eb
Bertolino <jgabbc@hotmail.com>
2016-10-18 21:30:40 -0300

    Correção de bugs e erros na função realloc. Reformatação da apresentação dos menus e contextos. Alteração da função scanf para pegastr quando solicitadas strings.

--- atividadesEmC/miniMaratonaFran/equipe.c
+++ atividadesEmC/miniMaratonaFran/equipe.c
@@ -1,9 +1,9 @@
-#include "string.variavel.h"
+#include "../string.variavel.h"
 
 char x;
 
 typedef struct{
-	char nomeAtv[1024];
+	char *nomeAtv;
 	short status;
 	unsigned int horasGast;
 } afazeres;
@@ -13,7 +13,6 @@ afazeres *atividades;
 short cadastrar( unsigned int qtd );
 void editar( unsigned int qtd );
 void excluir( unsigned int qtd );
-void relatorio( unsigned int qtd );
 
 void printaTudo( unsigned int qtd , short estado );
 
@@ -27,32 +26,34 @@ int main( void ){
 
 	do{
 
+		printf( "\n----------------------------------------------\n" );
 		printf( "Menu:\n" );
 		printf( "\tOpcao 1 : Cadastrar\n" );
 		printf( "\tOpcao 2 : Editar\n" );
 		printf( "\tOpcao 3 : Excluir\n" );
 		printf( "\tOpcao 4 : Relatorio\n" );
-		printf( "--Para sair digite 0: \n" );
+		printf( "\n## Para sair digite 0: \n" );
+		printf( "----------------------------------------------\n" );
 
-		printf( "Escolha a opcao: " );
-		scanf( "%d" , &opcao );
-
-	   	while( x != '\n' && x != EOF ) x = getc( stdin ) ;
+		printf( "\nEscolha a opcao: " );
+		scanf( "%u" , &opcao );
 
 		switch( opcao ){
 			case 1:
 				do{
-					atividades = realloc( atividades , ++cadastrados );
+					if( cadastrados + 1 >= 3 ) printf( "DEBUG\n" );
+					atividades = (afazeres *) realloc( atividades , ++cadastrados * sizeof( afazeres ) );
+					printf( "DEBUG: Passou realloc\n" );
 				}while( cadastrar( cadastrados ) != 0 );
 				break;
 			case 2:
 				editar( cadastrados );
 				break;
 			case 3:
-				excluir( cadastrados );
+				excluir( cadastrados-- );
 				break;
 			case 4:
-				relatorio( cadastrados );
+				printaTudo( cadastrados , 1 );
 				break;
 		}
 
@@ -65,15 +66,13 @@ short cadastrar( unsigned int qtd ){
 
 	char querer = 'a';
 
-	printf( "\n\tDigite o nome da atividade: " );
-	scanf( "%s" , atividades[qtd-1].nomeAtv );
-
-   	while( x != '\n' && x != EOF ) x = getc( stdin ) ;
+	printf( "\nDigite o nome da atividade: " );
+	pegastr( &(atividades[qtd-1].nomeAtv) );
 
 	atividades[qtd-1].status = 0;
 
-	printf( "\n\tDeseja cadastrar outra atividade? (S/N)" );
-	while( querer != 's' && querer != 'S' && querer != 'n' && querer != 'N' ) { scanf( "%c" , &querer ); while( x != '\n' && x != EOF ) x = getc( stdin ) ;};
+	printf( "\nDeseja cadastrar outra atividade? (S/N)" );
+	while( querer != 's' && querer != 'S' && querer != 'n' && querer != 'N' ) scanf( " %c" , &querer );
 
 	if( querer == 's' || querer == 'S' ) return 1 ;
 	else return 0;
@@ -86,10 +85,10 @@ void editar( unsigned int qtd ){
 
 	printaTudo( qtd , 2 );
 
-	printf( "\n\tDigite qual o ID da atividade deseja editar: " );
+	printf( "\nDigite qual o ID da atividade deseja editar: " );
 	scanf( "%u" , &id  );
 
-	printf( "\n\tAtividade %u : 'quantas horas foram gastas para sua resolução?' " , id );
+	printf( "\nAtividade %u : 'quantas horas foram gastas para sua resolução?' " , id );
 	scanf( "%u" , &atividades[id-1].horasGast );
 
 	atividades[id-1].status = 1;
@@ -102,43 +101,43 @@ void excluir( unsigned int qtd ){
 
 	printaTudo( qtd , 2 );
 
-	printf( "\n\tDigite qual o ID da atividade deseja excluir: " );
+	printf( "\nDigite qual o ID da atividade deseja excluir: " );
 	scanf( "%u" , &id  );
 
 	limpaVet( id - 1 , qtd );
 
 }
-void relatorio( unsigned int qtd ){
-	printaTudo( qtd , 1 );
-}
 
 void printaTudo( unsigned int qtd , short estado ){
 
 	unsigned int j = 0 , jTot = 0 , jY = 0;
+
+	if( qtd <= 0 ){ printf( "\n\n####### NÂO EXISTEM TAREDAS CADASTRADAS! #######\n\n" ); return; }
+
 	switch( estado ){
 		case 1:
 
 			while( j < qtd ){
 				if( atividades[j].status ){
-					printf( "\n-----------------------" );
+					printf( "\n---------------------------------------------------------------------" );
 					printf( "\n\tID: %u" , j + 1 );
 					printf( "\n\tNome: %s" , atividades[j].nomeAtv );
-					printf( "\n\tStatus: %s" , ( atividades[j].status == 0 ) ? "TODO" : "DONE" );
-					printf( "\n-----------------------" );
+					printf( "\n\tStatus: %s" , ( atividades[j].status == 0 ) ? "TO-DO" : "DONE" );
+					printf( "\n---------------------------------------------------------------------" );
 				}
 				++j;
 			}
 			j = 0;
 			while( j < qtd ){ if( atividades[j].status ) jTot += atividades[j].horasGast , ++jY; ++j; }
-			printf("\n\tForam feitas %u atividades! (Parabéns!)\n\tQuantidade de horas gastas: %u\n\n", jY , jTot );
+			printf("\n****Foram feitas %u atividades! (Parabéns!) ****\n\tQuantidade de horas gastas: %u\n\n", jY , jTot );
 			break;
 		case 2:
 			while( qtd > 0 ){
-				printf( "-----------------------" );
+				printf( "\n---------------------------------------------------------------------" );
 				printf( "\n\tID: %u" , qtd );
 				printf( "\n\tNome: %s" , atividades[qtd-1].nomeAtv );
-				printf( "\n\tStatus: %s" , ( atividades[qtd-1].status == 1 ) ? "TODO" : "DONE" );
-				printf( "-----------------------" );
+				printf( "\n\tStatus: %s" , ( atividades[qtd-1].status == 0 ) ? "TO-DO" : "DONE" );
+				printf( "\n---------------------------------------------------------------------" );
 				--qtd;
 			}	
 			break;
@@ -152,10 +151,10 @@ void limpaVet( unsigned int pos , unsigned int qtd ){
 	int i = 0;
 
 	for( i = pos ; i < qtd ; ++i){
-		printf( "DEBUGGGGGGG\n" );
+		
 		if( i != qtd - 1) atividades[i] = atividades[i+1];
 	}
 
-	atividades = realloc( atividades , qtd );
+	atividades = (afazeres *) realloc( atividades , qtd * sizeof( afazeres ) );
 
 }
